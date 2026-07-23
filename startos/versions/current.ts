863c7ec426f23f7faa7f5b1c06aad7c63fa5b6ad
syedasaadbilal-afk <syedasaadbilal@gmail.com>
2026-07-23 23:30:20 +0300

    Update current.ts

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,7 +1,7 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '1.18.0:0',
+  version: '1.18.0:1',
   releaseNotes: {
     en_US: 'Initial StartOS packaging of upstream hashrate-autopilot 1.18.0.',
   },
