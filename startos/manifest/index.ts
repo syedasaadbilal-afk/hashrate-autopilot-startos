a592a6e32791873ebf1109027dde2a0a8fc0a84b
syedasaadbilal-afk <syedasaadbilal@gmail.com>
2026-07-27 06:19:58 +0300

    Update index.ts

--- startos/manifest/index.ts
+++ startos/manifest/index.ts
@@ -13,7 +13,7 @@ export const manifest = setupManifest({
   volumes: ['main'],
   images: {
     autopilot: {
-      source: { dockerTag: 'ghcr.io/syedasaadbilal-afk/hashrate-autopilot:1.18.12' },
+      source: { dockerTag: 'ghcr.io/syedasaadbilal-afk/hashrate-autopilot:1.18.13' },
       arch: ['x86_64', 'aarch64'],
     },
   },
