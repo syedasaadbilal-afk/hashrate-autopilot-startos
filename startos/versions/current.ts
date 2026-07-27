296d32f2fbf91cb710597545f7a652422574c991
syedasaadbilal-afk <syedasaadbilal@gmail.com>
2026-07-27 20:57:26 +0300

    Update current.ts

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,7 +1,7 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '1.18.14:1',
+  version: '1.18.15:1',
   releaseNotes: {
     en_US: 'Dual-provider NiceHash: LIVE trading + in-app keys and config.',
   },
